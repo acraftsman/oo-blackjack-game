b02c31ae9704f7e6a545f24129d0a76018ff655d
Alan Hu <alanhu1988@gmail.com>
2015-04-18 22:25:24 +0800

    modified

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -1,8 +1,9 @@
-# Description: There are two to eight people in this game, a dealer and seven players at most. And there are one or more decks. At first, dealer respectively deal two cards to players. According to the order, player choose to hit or stay. If player choose to hit, and finally his/her total points great than 21, player bursted. If player choose to stay, current total points are saved to compare.
+# Description: There are two to eight people in this game, a dealer and seven players at most. And there are one or more decks. At first, dealer respectively deal two cards to players. According to the order, player choose to hit or stay. If player choose to hit, and finally his/her total points great than 21, player busted. If player choose to stay, current total points are saved to compare.
 
-# If dealer's total points great than or equal to 17, he/she must choose to stay. Otherwise, if dealer's total points less than 17, he or she must choose to hit. If dealer's total points great than 21, dealer bursted. And all players that haven't bursted won.
+# If dealer's total points great than or equal to 17, he/she must choose to stay. Otherwise, if dealer's total points less than 17, he or she must choose to hit. If dealer's total points great than 21, dealer busted. And all players that haven't busted won.
 
 # Finally, Dealer compare with every player's total points. 
+require "rubygems"
 require "pry"
 module Calculatable
   def calculate_total_points(cards)
@@ -29,26 +30,27 @@ module Calculatable
 
   def calculate_bet(dealer, players)
     players.each do |player|
-      if player.result_info[:blackjack]
+      # binding.pry
+      if player.status_info[:blackjack]
         if dealer.is_blackjack
           player.asset += player.bet
-          player.result_info[:assect_change] = "0$"
+          player.result_info[:asset_change] = "0$"
         else
           dealer.asset -= player.bet * 1.5
           player.asset += player.bet * 2.5
-          player.result_info[:assect_change] = "+#{player.bet * 1.5}$"
+          player.result_info[:asset_change] = "+#{player.bet * 1.5}$"
         end
       else
         if player.result_info[:result] == "win"
           dealer.asset -= player.bet
           player.asset += player.bet * 2
-          player.result_info[:assect_change] = "+#{player.bet}$"
+          player.result_info[:asset_change] = "+#{player.bet}$"
         elsif player.result_info[:result] == "lose"
           dealer.asset += player.bet
-          player.result_info[:assect_change] = "-#{player.bet}$"
+          player.result_info[:asset_change] = "-#{player.bet}$"
         elsif player.result_info[:result] == "tie"
           player.asset += player.bet
-          player.result_info[:assect_change] = "0$"
+          player.result_info[:asset_change] = "0$"
         else
           puts "There is a error occured in method: calculate_bet"
         end
@@ -80,15 +82,15 @@ class Deck
     count.times{ self.cards += SUIT.product(CARDS) }
   end
 
-  def shuffle_decks
+  def scramble_decks!
     self.cards.shuffle!
   end
 end
 
 class Dealer
   include Calculatable
-  attr_accessor :asset, :decks, :cards, :total, :is_bursted, :is_blackjack
-  def initialize(decks, asset = 1000000)
+  attr_accessor :asset, :decks, :cards, :calculate_total_points, :is_bursted, :is_blackjack
+  def initialize(decks, asset = 100000000)
     self.asset = asset
     self.decks = decks 
     self.cards = []
@@ -98,10 +100,17 @@ class Dealer
   end
 
   def deal(player)
-    unless player.nil?
-      player.cards << decks.cards.pop 
+    if !player.nil?
+      card = decks.cards.pop 
+      if card.nil?
+        decks = Deck.new
+        decks.scramble_decks!
+      else
+        player.cards << card
+      end
     end
     player.total = calculate_total_points(player.cards)
+    card
   end
 
   def choose
@@ -125,21 +134,21 @@ end
 class Player
   include Calculatable
   include Message
-  attr_accessor :name, :cards, :asset, :bet, :total, :result_info, :is_hit
+  attr_accessor :name, :cards, :asset, :bet, :total, :result_info, :status_info
   def initialize(name, asset = 500)
     self.name = name
     self.cards = []
     self.asset = asset
     self.bet = 0
     self.total = 0
-    self.result_info = {burst: false, result: "", blackjack: false, assect_change: "", chose_double: false}
-    self.is_hit = false
+    self.result_info = {result: "", assect_change: ""}
+    self.status_info = {bust: false, blackjack: false, choose_double: false, choose_hit: false}
   end
 
   def choose
     str_arr = ["Choose to 1)Hit 2)Stay 3)Double : ", "Choose to 1)Hit 2)Stay : "]
     index = 0
-    if is_hit
+    if status_info[:choose_hit]
       index = 1
     end
 
@@ -148,7 +157,7 @@ class Player
       hit_or_stay = gets.chomp.to_i
     end until [1, 2, 3].include?hit_or_stay
     if hit_or_stay == 1
-      self.is_hit = true
+      self.status_info[:choose_hit] = true
     end
     hit_or_stay
   end
@@ -166,12 +175,20 @@ class Player
     end
     self.asset -= bet
   end
+
+  def status_clear
+    self.cards = []
+    self.bet = 0
+    self.total = 0
+    self.result_info = {result: "", ssect_change: ""}
+    self.status_info = {bust: false, blackjack: false, choose_double: false, choose_hit: false}
+  end
 end
 
 class Game
   include Calculatable
   include Message
-  attr_accessor :decks, :dealer, :players, :result_info, :all_player_name
+  attr_accessor :decks, :dealer, :players, :all_player_name
   
   def initialize
     system "clear"
@@ -188,7 +205,7 @@ class Game
       self.all_player_name << player.name
     end
     self.decks = Deck.new(players_count)
-    decks.shuffle_decks
+    decks.scramble_decks!
     self.dealer = Dealer.new(decks)
   end
 
@@ -207,6 +224,24 @@ class Game
       end
     end
     # Turing to player
+    turning_to_player
+    # Turing to dealer
+    dealer.choose
+    # compare, calculate and announce result
+    compare_with_dealer
+    calculate_bet(dealer,players)
+    display_bet_info
+    # clear player
+    players.each do |player|
+      if player.asset == 0
+        announce "sorry, #{player.name} have not enough money! #{player.name} out!"
+        all_player_name.delete(player.name)
+      end
+    end
+    players.select!{ |player| all_player_name.include?player.name }
+  end
+
+  def turning_to_player
     players.each do |player|
       puts ""
       announce "Turning to #{player.name}..."
@@ -216,7 +251,7 @@ class Game
       player.total = calculate_total_points(player.cards)
       # blackjack
       if player.total == 21
-        player.result_info[:blackjack] = true
+        player.status_info[:blackjack] = true
         announce "#{player.name} blackjack!"
         next
       end
@@ -230,33 +265,33 @@ class Game
           player.total = calculate_total_points(player.cards)
           sleep(0.3)
           if player.total > 21
-            player.result_info[:burst] = true
-            announce "#{player.name} bursted! (total: #{player.total})"
+            player.status_info[:bust] = true
+            announce "#{player.name} busted! (total: #{player.total})"
             break
           elsif player.total == 21
-            player.result_info[:blackjack] = true
+            player.status_info[:blackjack] = true
             announce "#{player.name} blackjack!"
             break
           else
             next
           end
         when 2
-          player.result_info[:blackjack] = true if player.total == 21
+          player.status_info[:blackjack] = true if player.total == 21
           announce "Saving #{player.name}'s total..."
           sleep(0.5)
           break
         when 3
-          player.result_info[:chose_double] = true
+          player.status_info[:choose_double] = true
           if player.asset >= player.bet
             player.asset -= player.bet
             player.bet = 2 * player.bet
             dealer.deal(player)
             announce "#{player.name}'s cards: #{player.cards}"
             if player.total > 21
-              player.result_info[:burst] = true
-              announce "#{player.name} bursted! (total: #{player.total})"
+              player.status_info[:bust] = true
+              announce "#{player.name} busted! (total: #{player.total})"
             elsif player.total == 21
-              player.result_info[:blackjack] = true
+              player.status_info[:blackjack] = true
               announce "#{player.name} blackjack!"
             end
           else
@@ -267,32 +302,20 @@ class Game
         end
       end
     end
-    dealer.choose
-    compare_with_dealer
-    calculate_bet(dealer,players)
-    display_bet_info
-    players.each do |player|
-      if player.asset == 0
-        announce "sorry, #{player.name} have not enough money! #{player.name} out!"
-        all_player_name.delete(player.name)
-      end
-    end
-    players.select!{ |player| all_player_name.include?player.name }
   end
-
 
   def compare_with_dealer
     puts ""
     puts "***** ANNONCING RESULT *****"
     players.each do |player|
-      if dealer.is_bursted == true && player.result_info[:burst] == false
+      if dealer.is_bursted == true && player.result_info[:bust] == false
         player.result_info[:result] = "win"
-        announce  "#{result_msg(player)} Dealer bursted!"
-      elsif dealer.is_bursted == true && player.result_info[:burst] == true
+        announce  "#{result_msg(player)} Dealer busted!"
+      elsif dealer.is_bursted == true && player.result_info[:bust] == true
         player.result_info[:result] = "tie"
-        announce "#{result_msg(player)} You two all bursted!"
+        announce "#{result_msg(player)} You two all busted!"
       else
-        if !player.result_info[:burst]
+        if !player.result_info[:bust]
           if player.total > dealer.total
             player.result_info[:result] = "win"  
             announce result_msg(player)
@@ -323,30 +346,21 @@ class Game
     puts ""
     puts "***** BET INFO *****"
     players.each do |player|
-      announce "#{player.name}: #{player.result_info[:assect_change]}. Current asset: #{player.asset}$"
+      announce "#{player.name}: #{player.result_info[:asset_change]}. Current asset: #{player.asset}$"
     end
   end
 
   def reset
     system "clear"
     self.decks = Deck.new(players.size)
-    decks.shuffle_decks
+    decks.scramble_decks!
     dealer.decks = decks
     dealer.cards = []
     dealer.total = 0
     dealer.is_bursted = false
     dealer.is_blackjack = false
     players.each do |player|
-      player.cards = []
-      player.bet = 0
-      player.total = 0
-      player.result_info = {burst: false, result: "", blackjack: false, assect_change: ""}
-    end
-  end
-
-  def status_clear
-    players.each do |player|
-      player.is_hit = false
+      player.status_clear
     end
   end
 end
@@ -362,6 +376,5 @@ loop do
   yes_or_no = gets.chomp
   break unless yes_or_no == 'y'
   game.reset
-  game.status_clear
 end
 
